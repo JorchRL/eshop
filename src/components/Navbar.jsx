8fb1d7982a7c87a4d60282005af457a54c89e76a
Jorch <jorch.rl@gmail.com>
2021-11-30 14:24:21 -0600

    Finish Navbar component styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,94 @@
 import React from "react";
+import styled from "styled-components";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import Badge from "@material-ui/core/Badge";
+
+const Container = styled.div`
+  height: 60px;
+  /* background-color: black; */
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+// LEFT SIDE OF NAVBAR
+const Left = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+`;
+const Language = styled.span`
+  font-size: 18px;
+  cursor: pointer;
+`;
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+
+  cursor: pointer;
+`;
+const Input = styled.input`
+  border: none;
+`;
+// ^^^^^^
+
+// CENTER OF NAVBAR
+const Center = styled.div`
+  flex: 2;
+  text-align: center;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+// ^^^^^^
+
+// RIGHT SIDE OF NAVBAR
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+const MenuItem = styled.div`
+  font-size: 14px;
+  margin-left: 25px;
+  cursor: pointer;
+`;
+// ^^^^^^
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{ fontSize: "16px", color: "grey" }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>KATO.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={3} color={"primary"}>
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
