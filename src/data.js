6b24f0ee23e2cf39ba21e314c3284e5d51d68d8c
Jorch <jorch.rl@gmail.com>
2021-12-01 14:50:55 -0600

    Add data.js and imgs

---
+++ src/data.js
@@ -0,0 +1,27 @@
+import img1 from "./imgs/cato1.jpg";
+import img2 from "./imgs/cato2.jpg";
+import img3 from "./imgs/cato3.jpg";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: img1,
+    title: "summer sale",
+    desc: "don't compromise on style! get flat 30% off for new arrivals.",
+    bg: "#fff",
+  },
+  {
+    id: 2,
+    img: img2,
+    title: "spring sale",
+    desc: "don't compromise on style! get flat 30% off for new arrivals.",
+    bg: "#fff",
+  },
+  {
+    id: 3,
+    img: img3,
+    title: "winter sale",
+    desc: "don't compromise on style! get flat 30% off for new arrivals.",
+    bg: "#fff",
+  },
+];
