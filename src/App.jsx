d5c8e59ce3ede2b2a5b7b0aa7bf09c746b144e91
Jorch <jorch.rl@gmail.com>
2021-12-04 13:18:38 -0600

    Add Cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
 import Product from "./pages/Product";
 
 const App = () => {
-  return <Product />;
+  return <Cart />;
 };
 
 export default App;
